bc3ba77031456174c61fc244e5122072332605b9
Jasper van Mensel <jvanmensel@xpirit.com>
2023-05-23 16:11:49 +0200

    additional benchmark calculations

--- TestDataGeneration.Benchmarks/Program.cs
+++ TestDataGeneration.Benchmarks/Program.cs
@@ -2,44 +2,12 @@
 using BenchmarkDotNet.Running;
 using Bogus;
 using TestDataGeneration;
+using TestDataGeneration.DemoDomain;
 
 Console.WriteLine("Hello, World!");
-var summary = BenchmarkRunner.Run<SomeVsFakerRandomVsReusedFakerRandom>();
+var summary = BenchmarkRunner.Run<SomeIntVsFakerRandomIntVsReusedFakerRandomIntVsSomeGeneratedIntVsFakerGenerateIntVsReusedFakerGenerateInt>();
+//var summary = BenchmarkRunner.Run(typeof(Program).GetAssembly());
 Console.WriteLine("Bye, World!");
-
-//Benchmark comparing Some.Int() vs Faker.Random.Int()
-public class SomeVsFakerRandom
-{
-    [Benchmark]
-    public int SomeInt() => Some.Int();
-
-    [Benchmark]
-    public int FakerRandomInt() => new Faker().Random.Int();
-}
-
-//Benchmark comparing Some.Int() vs reused Faker.Random.Int()
-public class SomeVsReusedFakerRandom
-{
-    private readonly Faker _faker = new Faker();
-
-    [Benchmark]
-    public int SomeInt() => Some.Int();
-
-    [Benchmark]
-    public int ReusedFakerRandomInt() => _faker.Random.Int();
-}
-
-//Benchmark comparing Faker.Random.Int() vs reused Faker.Random.Int()
-public class FakerRandomVsReusedFakerRandom
-{
-    private readonly Faker _faker = new Faker();
-
-    [Benchmark]
-    public int FakerRandomInt() => new Faker().Random.Int();
-
-    [Benchmark]
-    public int ReusedFakerRandomInt() => _faker.Random.Int();
-}
 
 //Benchmark comparing Some.Int() vs Faker.Random.Int() vs reused Faker.Random.Int()
 public class SomeVsFakerRandomVsReusedFakerRandom
@@ -56,3 +24,50 @@ public class SomeVsFakerRandomVsReusedFakerRandom
     public int ReusedFakerRandomInt() => _faker.Random.Int();
 }
 
+//Benchmark comparing Some.Generated<Article>() vs Faker<Article>().Generate() vs reused Faker<Article>().Generate()
+public class SomeGeneratedVsFakerGenerateVsReusedFakerGenerate
+{
+    private readonly Faker<Article> _faker = new Faker<Article>();
+
+    [Benchmark]
+    public Article SomeGeneratedArticle() => Some.Generated<Article>();
+
+    [Benchmark]
+    public Article FakerGenerateArticle() => new Faker<Article>().Generate();
+
+    [Benchmark]
+    public Article ReusedFakerGenerateArticle() => _faker.Generate();
+}
+
+//Benchmark comparing Some.Generated<Article>(count) vs Faker<Article>().Generate(count) vs reused Faker<Article>().Generate(count)
+public class SomeGeneratedCountVsFakerGenerateCountVsReusedFakerGenerateCount
+{
+    private readonly Faker<Article> _faker = new Faker<Article>();
+
+    [Benchmark]
+    public List<Article> SomeGeneratedArticles() => Some.Generated<Article>(10);
+
+    [Benchmark]
+    public List<Article> FakerGenerateArticles() => new Faker<Article>().Generate(10);
+
+    [Benchmark]
+    public List<Article> ReusedFakerGenerateArticles() => _faker.Generate(10);
+}
+
+//Benchmark comparing Some.Int() vs Faker.Random.Int() vs reused Faker.Random.Int() vs Some.Generated<int>()
+public class SomeIntVsFakerRandomIntVsReusedFakerRandomIntVsSomeGeneratedIntVsFakerGenerateIntVsReusedFakerGenerateInt
+{
+    private readonly Faker _faker = new Faker();
+
+    [Benchmark]
+    public int SomeInt() => Some.Int();
+
+    [Benchmark]
+    public int FakerRandomInt() => new Faker().Random.Int();
+
+    [Benchmark]
+    public int ReusedFakerRandomInt() => _faker.Random.Int();
+
+    [Benchmark]
+    public int SomeGeneratedInt() => Some.Generated<int>();
+}
