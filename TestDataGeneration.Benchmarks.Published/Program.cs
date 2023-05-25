6462f57b0a3262ac6b88ba560442c37196c08afb
Jasper van Mensel <jvanmensel@xpirit.com>
2023-05-25 15:21:07 +0200

    additional benchmarks on custom type generations

--- TestDataGeneration.Benchmarks.Published/Program.cs
+++ TestDataGeneration.Benchmarks.Published/Program.cs
@@ -5,7 +5,7 @@ using Bogus.Platform;
 using TestDataGeneration.Benchmarks.Published;
 
 Console.WriteLine("Hello, Published World!");
-var summary = BenchmarkRunner.Run<IntGenerationBenchmarks>();
+var summary = BenchmarkRunner.Run<CustomTypeGenerationBenchmarks>();
 //var summary = BenchmarkRunner.Run(typeof(Program).GetAssembly());
 Console.WriteLine("Bye, Published World!");
 
