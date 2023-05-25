7f6c29bfbef4089d2551a21a47cbab7e2c54f3aa
Jasper van Mensel <jvanmensel@xpirit.com>
2023-05-25 15:01:23 +0200

    tests to verify default rules are applied correctly

---
+++ TestDataGeneration.UnitTests/Wiring/TestBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Bogus;
+using TestDataGeneration.UnitTests.TestObjects;
+
+namespace TestDataGeneration.UnitTests.Wiring;
+
+public class TestBinder : Some.DefaultBinder
+{
+    public TestBinder()
+    {
+        TypeRules[typeof(DummyObject)] = ArticleRules;
+    }
+
+    private static readonly Func<Faker<DummyObject>, Faker<DummyObject>> ArticleRules =
+        f => f.RuleFor(x => x.GuidWithRuleFor, DummyObject.GuidValue);
+}
