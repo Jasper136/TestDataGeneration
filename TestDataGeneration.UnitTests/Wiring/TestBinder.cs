1ef62bcee8a803f17bd48a2081d85f57051cd686
Jasper van Mensel <jvanmensel@xpirit.com>
2023-05-31 11:25:19 +0200

    pinpointing an issue

--- TestDataGeneration.UnitTests/Wiring/TestBinder.cs
+++ TestDataGeneration.UnitTests/Wiring/TestBinder.cs
@@ -9,8 +9,15 @@ public class TestBinder : Some.DefaultBinder
     public TestBinder()
     {
         TypeRules[typeof(DummyObject)] = DummyObjectRules;
+
+        //todo: avoid this, when no rules are needed, you should not have to set them (source generator??)
+        TypeRules[typeof(DummyObjectWithoutDefaultRules)] = DummyObjectWithoutDefaultRulesRules;
     }
 
     private static readonly Func<Faker<DummyObject>, Faker<DummyObject>> DummyObjectRules =
         f => f.RuleFor(x => x.GuidWithRuleFor, DummyObject.GuidValue);
+
+    //todo: avoid this, when no rules are needed, you should not have to set them (source generator??)
+    private static readonly Func<Faker<DummyObjectWithoutDefaultRules>, Faker<DummyObjectWithoutDefaultRules>> DummyObjectWithoutDefaultRulesRules =
+        f => f;
 }
